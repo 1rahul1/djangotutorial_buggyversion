333c7e7a0e91adb20fff2f456dd6ba35ecec27f5
Rahul Prasad <1rahulprasad1@gmail.com>
2018-05-04 20:00:09 +0530

    updated the admmin.py

--- ratemytagline/admin.py
+++ ratemytagline/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import tagLine
 
 #I am not able to see tagLine in the administrator console. Can you make it visible in the admin console?
+
+from django.contrib import admin
+from .models import Post
+admin.site.register(Post)
